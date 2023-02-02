a7de783c6a76691f74451b38fe6f7e0d8e380cbf
serraguti <pacoserranox@gmail.com>
2023-02-02 19:02:11 +0100

    commit

---
+++ MvcApiPersonajesSeries2023/Controllers/SeriesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApiPersonajesSeries2023.Models;
+using MvcApiPersonajesSeries2023.Services;
+
+namespace MvcApiPersonajesSeries2023.Controllers
+{
+    public class SeriesController : Controller
+    {
+        private ServiceSeries service;
+
+        public SeriesController(ServiceSeries service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Serie> series = await this.service.GetSeriesAsync();
+            return View(series);
+        }
+
+        public async Task<IActionResult> Details(int idserie)
+        {
+            Serie serie = await this.service.FindSerieAsync(idserie);
+            return View(serie);
+        }
+    }
+}
