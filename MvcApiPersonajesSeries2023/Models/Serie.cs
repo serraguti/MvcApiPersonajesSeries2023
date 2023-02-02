55ec0423a8531f6e147d1d23d0b0ba98bad7ee49
serraguti <pacoserranox@gmail.com>
2023-02-02 18:55:01 +0100

    Add project files.

---
+++ MvcApiPersonajesSeries2023/Models/Serie.cs
@@ -0,0 +1,10 @@
+﻿namespace MvcApiPersonajesSeries2023.Models
+{
+    public class Serie
+    {
+        public int IdSerie { get; set; }
+        public string Nombre { get; set; }
+        public string Imagen { get; set; }
+        public int Anyo { get; set; }
+    }
+}
---
+++ MvcApiPersonajesSeries2023/Services/ServiceSeries.cs
@@ -0,0 +1,114 @@
+﻿using MvcApiPersonajesSeries2023.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace MvcApiPersonajesSeries2023.Services
+{
+    public class ServiceSeries
+    {
+        private string UrlApi;
+
+        private MediaTypeWithQualityHeaderValue header;
+
+        public ServiceSeries(string url)
+        {
+            this.UrlApi = url;
+            this.header = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+        private async Task<T> CallApiAsync<T>(string request)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+                HttpResponseMessage response =
+                    await client.GetAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    T data = await response.Content.ReadAsAsync<T>();
+                    return data;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+
+        #region SERIES
+
+        public async Task<List<Serie>> GetSeriesAsync()
+        {
+            string request = "/api/series";
+            List<Serie> series = await this.CallApiAsync<List<Serie>>(request);
+            return series;
+        }
+
+        public async Task<Serie> FindSerieAsync(int idserie)
+        {
+            string request = "/api/series/" + idserie;
+            Serie serie = await this.CallApiAsync<Serie>(request);
+            return serie;
+        }
+
+        #endregion
+
+        #region PERSONAJES
+
+        public async Task<List<Personaje>> GetPersonajesAsync()
+        {
+            string request = "/api/personajes";
+            List<Personaje> personajes = await this.CallApiAsync<List<Personaje>>(request);
+            return personajes;
+        }
+
+        public async Task<Personaje> FindPersonajeAsync(int idpersonaje)
+        {
+            string request = "/api/personajes/" + idpersonaje;
+            Personaje personaje = await this.CallApiAsync<Personaje>(request);
+            return personaje;
+        }
+
+        public async Task CreatePersonajeAsync
+            (int id, string nombre, string imagen, int idserie)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "/api/personajes";
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Clear();
+                Personaje personaje = new Personaje();
+                personaje.IdPersonaje = id;
+                personaje.Nombre = nombre;
+                personaje.Imagen = imagen;
+                personaje.IdSerie = idserie;
+                string json =
+                    JsonConvert.SerializeObject(personaje);
+                StringContent content =
+                    new StringContent(json, Encoding.UTF8, "application/json");
+                await client.PostAsync(request, content);
+            }
+        }
+
+        public async Task UpdateSeriePersonajeAsync(int idpersonaje, int idserie)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "/api/personajes/updatepersonajeserie/"
+                    + idpersonaje + "/" + idserie;
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Clear();
+                //AUNQUE NO CREEMOS NADA, DEBEMOS ENVIAR UN OBJETO
+                //VACIO Content EN LA PETICION DEL PUT
+                StringContent content =
+                    new StringContent("", Encoding.UTF8, "application/json");
+                await client.PutAsync(request, content);
+            }
+        }
+
+        #endregion
+    }
+}
