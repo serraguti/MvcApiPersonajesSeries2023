5d8d71809a765e48cc0b4a01cd1eae518d87eb89
serraguti <pacoserranox@gmail.com>
2023-02-03 16:55:32 +0100

    commit

---
+++ MvcApiPersonajesSeries2023/ViewComponents/MenuSeriesViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApiPersonajesSeries2023.Models;
+using MvcApiPersonajesSeries2023.Services;
+
+namespace MvcApiPersonajesSeries2023.ViewComponents
+{
+    public class MenuSeriesViewComponent: ViewComponent
+    {
+        private ServiceSeries service;
+
+        public MenuSeriesViewComponent(ServiceSeries service)
+        {
+            this.service = service;
+        }
+
+        //EL METODO InvokeAsync() ES EL ENCARGADO DE ENVIAR UN 
+        //MODEL HACIA NUESTRO LAYOUT.
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<Serie> series = await this.service.GetSeriesAsync();
+            return View(series);
+        }
+    }
+}
